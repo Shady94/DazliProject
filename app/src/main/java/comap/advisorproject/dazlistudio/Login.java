11c5d9a08276044ea288da8d6e7691abed06334d
Shady94 <jgo.uach@gmail.com>
2016-02-04 23:18:50 -0700

    Login OK

--- app/src/main/java/comap/advisorproject/dazlistudio/Login.java
+++ app/src/main/java/comap/advisorproject/dazlistudio/Login.java
@@ -6,12 +6,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 
 public class Login extends Activity{
 
-   Button btn_login;
+    Button btn_login;
+    private  LoginButton loginButton;
+    CallbackManager callbackManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +34,29 @@ public class Login extends Activity{
             public void onClick(View v) {
                 Intent vis = new Intent(getApplicationContext(), Visualizador.class);
                 startActivity(vis);
+            }
+        });
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions("user_friends");
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
             }
         });
 
