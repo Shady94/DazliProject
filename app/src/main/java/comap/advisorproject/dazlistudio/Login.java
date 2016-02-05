017bcd93c866ef682de457c85838e2f06336dcde
Shady94 <jgo.uach@gmail.com>
2016-02-04 20:39:00 -0700

    Prueba exitosa

--- app/src/main/java/comap/advisorproject/dazlistudio/Login.java
+++ app/src/main/java/comap/advisorproject/dazlistudio/Login.java
@@ -1,10 +1,15 @@
 package comap.advisorproject.dazlistudio;
 
 import android.app.Activity;
+import android.os.Bundle;
 
-/**
- * Created by zshady on 2/02/16.
- */
 public class Login extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
 
 }
