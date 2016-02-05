060fe2db5d84c18fc1b3c2cdae97860b8aaf7312
Shady94 <jgo.uach@gmail.com>
2016-02-04 21:52:11 -0700

    Configurado el boton para llegar al visualizador

--- app/src/main/java/comap/advisorproject/dazlistudio/Login.java
+++ app/src/main/java/comap/advisorproject/dazlistudio/Login.java
@@ -1,14 +1,30 @@
 package comap.advisorproject.dazlistudio;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class Login extends Activity{
+
+   Button btn_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btn_login = (Button)findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent vis = new Intent(getApplicationContext(), Visualizador.class);
+                startActivity(vis);
+            }
+        });
 
     }
 
