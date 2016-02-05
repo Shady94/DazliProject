d9546f2663e0c8363271b80f69527e06e9cb9fef
Shady94 <jgo.uach@gmail.com>
2016-02-04 21:10:00 -0700

    Agregados layout y jc del visualizador

---
+++ app/src/main/java/comap/advisorproject/dazlistudio/Visualizador.java
@@ -0,0 +1,17 @@
+package comap.advisorproject.dazlistudio;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by zshady on 4/02/16.
+ */
+public class Visualizador extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_visualizador);
+
+    }
+}
